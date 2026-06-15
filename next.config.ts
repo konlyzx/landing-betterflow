7d04ad1d0077911979f005c90c31c0d8705b945b
konlyzx <kevinnazarit74@gmail.com>
2026-06-15 12:30:06 -0500

    remove www to non-www and HTTP to HTTPS redirects from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,35 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: { unoptimized: true },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  async redirects() {
-    return [
-      // Redirect www to non-www
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.betterflow.site',
-          },
-        ],
-        destination: 'https://betterflow.site/:path*',
-        permanent: true,
-      },
-      // Redirect HTTP to HTTPS
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: 'http',
-          },
-        ],
-        destination: 'https://betterflow.site/:path*',
-        permanent: true,
-      },
-    ];
-  },
   async headers() {
     return [
       {
